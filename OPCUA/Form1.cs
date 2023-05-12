1c767516291427c3a2bb25c1df092ec9c16f245f
Sanem Çiloğlu <92040170+Sanemciloglu@users.noreply.github.com>
2023-05-12 09:12:02 +0300

    Add project files.

---
+++ OPCUA/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Opc.UaFx.Client;
+
+namespace OPCUA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try { 
+            string opcUrl=textBox1.Text;
+            var tagName = textBox2.Text;
+            var client= new OpcClient(opcUrl);  
+            client.Connect();
+            var deger = client.ReadNode(tagName);
+            textBox3.Text=deger.ToString();
+            client.Disconnect();}
+            catch
+            {
+                MessageBox.Show("hata oluştu");
+            }
+            }
+    }
+}
